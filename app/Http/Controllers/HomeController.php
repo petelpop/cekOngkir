d16445c1e0bfc703992b358d668fcc0907d99782
ipal <72238450+petelpop@users.noreply.github.com>
2023-04-14 09:27:25 +0700

    adding rajaongkir library & implement

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Province;
 use App\Models\City;
 use App\Models\Courier;
 use Illuminate\Http\Request;
+use Kavist\RajaOngkir\Facades\RajaOngkir;
 
 class HomeController extends Controller
 {
@@ -33,8 +34,41 @@ class HomeController extends Controller
         return view('home', compact('province', 'courier'));
     }
 
+//     public function store(Request $request){
+//         // dd($request->all());
+//     $daftarProvinsi = RajaOngkir::ongkosKirim([
+//     'origin'        => $request -> origin,     // ID kota/kabupaten asal
+//     'destination'   => $request -> destination,      // ID kota/kabupaten tujuan
+//     'weight'        => 1300,    // berat barang dalam gram
+//     'courier'       => $request -> courier[0]    // kode kurir pengiriman: ['jne', 'tiki', 'pos'] untuk starter
+// ])->get();
+
+// dd($cost);
+//     }
+
     public function store(Request $request){
-        dd($request->all());
+        // dd($request->all());
+
+        $courier = $request->input('courier');
+
+        if ($courier) {
+            $result = [];
+
+            foreach ($courier as $value) {
+            $cost = RajaOngkir::ongkosKirim([
+                    'origin'        => $request -> city_origin,     // ID kota/kabupaten asal
+                    'destination'   => $request -> destination,      // ID kota/kabupaten tujuan
+                    'weight'        => 1300,    // berat barang dalam gram
+                    'courier'       => $value   // kode kurir pengiriman: ['jne', 'tiki', 'pos'] untuk starter
+                ])->get(); 
+
+                $result[] = $cost;
+
+            }
+
+            return $result;
+        }
+
     }
 
     public function getCourier(){
